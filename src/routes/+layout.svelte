0b8a01b3b0bf8543edf0d22a3754d05298ca1f0d
watcol <potfman@gmail.com>
2023-01-17 19:55:09 +0900

    ヘッダー、フッター、背景の位置改善

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <div class={containerStyle}>
-  <img style:--left="{-x}px" class={backgroundStyle} alt="background" src="{base}/img/pc-background.png">
+  <img class={backgroundStyle} alt="background" src="{base}/img/pc-background.png">
   <header class={headerStyle}>
     <Header/>
   </header>
@@ -11,45 +11,45 @@
   </footer>
 </div>
 
-<svelte:window bind:scrollX={x} />
-
 <script lang="ts">
   import 'destyle.css/destyle.min.css';
+  import '$lib/styles/global.css';
   import { base } from '$app/paths';
   import { css } from '@emotion/css';
   import Header from '$lib/header/Header.svelte';
   import Footer from '$lib/footer/Footer.svelte';
 
-  let x = 0;
-
   const backgroundStyle = css`
-    position: fixed;
-    overflow: hidden;
+    position: sticky;
+    object-fit: cover;
+    overflow: visible;
     z-index: -1;
-    top: 0;
-    left: var(--left);
-    min-width: 100%;
-    min-height: 100%;
+    top: 3rem;
+    bottom: 0;
+    width: 100%;
+    height: calc(100vh - 3rem);
+    margin-bottom: calc(-100vh + 3rem);
   `;
 
   const containerStyle = css`
     display: flex;
+    position: relative;
     flex-direction: column;
     width: max-content;
-    min-height: 100vh;
+    min-height: 100%;
     min-width: 100%;
   `;
 
   const headerStyle = css`
     position: sticky;
     min-width: 100%;
+    height: 100%;
     width: max-content;
     top: 0;
   `;
 
   const mainStyle = css`
     flex-grow: 1;
-    /* margin-top: 10rem; */
   `;
 
   const footerStyle = css`
