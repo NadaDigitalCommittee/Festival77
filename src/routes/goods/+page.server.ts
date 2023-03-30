3974319ebab40939dcf83bd88cac44fcdebfbeaf
watcol <potfman@gmail.com>
2023-03-30 17:18:15 +0900

    SSRされないのを直す

--- src/routes/goods/+page.server.ts
+++ src/routes/goods/+page.server.ts
@@ -1,6 +1,8 @@
 import type { PageServerLoad } from './$types';
 import client from '$lib/server/contentful';
 
+export const ssr = true;
+
 export const load = (async () => {
   const data = await client.getEntries({
     content_type: 'goods',
