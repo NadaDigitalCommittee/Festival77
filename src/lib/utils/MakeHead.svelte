f49c641e58c8713d41ad830c53ef24c7585c2ca5
watcol <potfman@gmail.com>
2023-04-04 16:09:26 +0900

    OGPを画像以外

---
+++ src/lib/utils/MakeHead.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description}>
+  <meta property="og:title" content={title}>
+  <meta property="og:description" content={description}>
+  <meta property="og:url" content={$page.url.toString()}>
+</svelte:head>
+
+<script lang="ts">  // headを生成する
+  import { page } from '$app/stores';
+
+  export let name: string | null = null;
+  export let description: string;
+
+  const baseTitle = '第77回灘校文化祭「Splash!」';
+  const title = name ? `${name} | ${baseTitle}` : baseTitle;
+</script>
--- src/routes/goods/+page.svelte
+++ src/routes/goods/+page.svelte
@@ -1,9 +1,11 @@
+<MakeHead name="グッズ" description="第77回灘校文化祭「Splash!」にて販売される灘校グッズの一覧です。(準備中)" />
 <ComingSoon/>
 <p>{JSON.stringify(data)}</p>
 
 <script lang="ts">
   import ComingSoon from '$lib/utils/ComingSoon.svelte';
   import type { PageData } from './$types';
+  import MakeHead from '$lib/utils/MakeHead.svelte';
 
   export let data: PageData;
 </script>
