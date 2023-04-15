50a5dac5ef044215590f8fe104d7a69886f55696
testuser1111111-nd <tbsa334zn6@outlook.jp>
2023-04-15 22:55:29 +0900

    lint1

--- src/routes/circles/+page.svelte
+++ src/routes/circles/+page.svelte
@@ -10,35 +10,35 @@
     <div class={floor} >
     <h2 class={headStyle}>{areaData.a}</h2>
     <ul class={listStyle}>
-      {#each items.filter((i)=>(i.areaId>=areaData.b && i.areaId< areaData.c)) as item }
+      {#each items.filter((i) => (i.areaId >= areaData.b && i.areaId < areaData.c)) as item }
         <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <li class="{liStyle} {item.selected2? liStyle2 : '' }" on:mouseenter={()=>{item.selected=true;}} on:mouseleave={()=>{item.selected=false;}} id={item.circleId} on:click={()=>{
-          if(item.selected2){
+        <li class="{liStyle} {item.selected2 ? liStyle2 : '' }" on:mouseenter={() => { item.selected = true; }} on:mouseleave={() => { item.selected = false; }} id={item.circleId} on:click={() => {
+          if (item.selected2) {
             item.selected2 = false;
             item.selected3 = false;
-          }else{
+          } else {
             item.selected2 = true;
-            setTimeout(()=>{item.selected3 = item.selected2&&true;},800);
+            setTimeout(() => { item.selected3 = item.selected2 && true; }, 800);
           }
           } }>
-          <div class="{unselectedStyle} {item.selected? selectedStyle : '' }"/>
+          <div class="{unselectedStyle} {item.selected ? selectedStyle : '' }"/>
           <div class={boxStyle}>
           <div class={itemStyle} >
             <div class={itemMainStyle}>
               <p class={contentStyle}>{item.name}</p>
-              {#if item.areaId >=9}
+              {#if item.areaId >= 9}
               <div class="{bigarea} {ken}">
                 <p class={kousya}>
                   研修館
                 </p>
               </div>
-              {:else if item.areaId%3 ==0}
+              {:else if item.areaId % 3 === 0}
               <div class="{bigarea} {tyu}">
                 <p class={kousya}>
                   中学棟
                 </p>
               </div>
-              {:else if item.areaId%3 ==1}
+              {:else if item.areaId % 3 === 1}
               <div class="{bigarea} {kou}" >
                 <p class={kousya}>
                   高校棟
@@ -53,7 +53,7 @@
               {/if}
               <p class={dateStyle}>{item.area}</p>
             </div>
-            <div class={item.selected2 ? arrowStyle2 :arrowStyle}>
+            <div class={item.selected2 ? arrowStyle2 : arrowStyle}>
               <span class={item.selected ? spanStyle : spanStyle2}/>
               <span class={item.selected ? spanStyle : spanStyle2}/>
             </div>
@@ -73,26 +73,12 @@
   import type { PageData } from './$types';
   import Title from '$lib/utils/Title.svelte';
   import MakeHead from '$lib/utils/MakeHead.svelte';
-  import { css, keyframes } from '@emotion/css';
-  import { colors,mobileOnly, pcOnly, responsive } from '$lib/styles/utils';
+  import { css } from '@emotion/css';
+  import {
+    colors, mobileOnly, pcOnly, responsive,
+  } from '$lib/styles/utils';
+  
   export let data: PageData;
-  const items = data.items as Circle[];
-  const areaDatas = [{a:"2F",b:0,c:3},{a:"3F",b:3,c:6},{a:"4F",b:6,c:9},{a:"研修館",b:9,c:12}];
-  let inputtext:string = "";
-  function runSearch(){
-    //名前との完全一致検索 連続部分文字列のほうがいいなら言って
-    let flag = false;
-    for(let i=0; i < items.length ; i++ ){
-      if(inputtext === items.at(i)?.name){
-        location.href= "./circles#"+items.at(i)?.circleId;
-        scrollBy(0,-125);
-        flag=true;
-      }
-    }
-    if(!flag){
-      alert("検索失敗");
-    }
-  }
 
   type Circle = {
       name: string;
@@ -104,7 +90,26 @@
       selected2: boolean;
       selected3: boolean;
   };
-    const spanStyle = css`
+
+  const items = data.items as Circle[];
+  const areaDatas = [{ a: '2F', b: 0, c: 3 }, { a: '3F', b: 3, c: 6 }, { a: '4F', b: 6, c: 9 }, { a: '研修館', b: 9, c: 12 }];
+  let inputtext = '';
+  function runSearch() {
+    // 名前との完全一致検索 連続部分文字列のほうがいいなら言って
+    let flag = false;
+    for (let i = 0; i < items.length; i += 1) {
+      if (inputtext === items.at(i)?.name) {
+        location.href = `./circles#${items.at(i)?.circleId}`;
+        scrollBy(0, -125);
+        flag = true;
+      }
+    }
+    if (!flag) {
+      alert('検索失敗');
+    }
+  }
+
+  const spanStyle = css`
     :nth-child(1){
     background-color: #0d3a4f;
     
@@ -115,7 +120,7 @@
     
     transition: 0.6s;
     }`;
-    const spanStyle2 = css`
+  const spanStyle2 = css`
     :nth-child(1){
     transition: 0.6s;
     }
@@ -123,12 +128,11 @@
     transition: 0.6s;
     }`;
 
-
-    const floor = css`
+  const floor = css`
     width:100%;
     margin-top:50px;
     `;
-    const searchBoxStyle = css`
+  const searchBoxStyle = css`
     width:400px;
     height:66px;
     border-radius:9999px;
@@ -137,11 +141,11 @@
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
     padding: 20px 33px;
     `;
-    const searchBoxImageStyle = css`
+  const searchBoxImageStyle = css`
     width:25px;
     height:25px;
     `;
-    const textinputStyle= css`
+  const textinputStyle = css`
       height:auto;
       width:285px;
       margin-left:15px;
@@ -154,7 +158,7 @@
         outline: none;
       }
     `;
-    const descStyle = css`
+  const descStyle = css`
     margin:0 10%;
     font-family: 'Source Han Sans JP';
     font-style: normal;
@@ -162,29 +166,29 @@
     font-size: 1rem;
     line-height: 23px;
     `;
-    const boxStyle = css`
+  const boxStyle = css`
     width:100%;
     `;
-    const liStyle = css`
+  const liStyle = css`
     display:flex;
     height:80px;
     transition: 0.6s;
     `;
-    const liStyle2 = css`
+  const liStyle2 = css`
     display:flex;
     height: 250px;
     transition: 0.6s;
     `;
-    const selectedStyle = css`
+  const selectedStyle = css`
     background-color:#0D3A4FFF;
     transition: 0.6s;
     `;
-    const unselectedStyle = css`
+  const unselectedStyle = css`
     width:2%;
     height: 100%;
     transition: 0.6s;
     `;
-    const bigarea = css`
+  const bigarea = css`
     margin-top: -3px;
     width: 11.5%;
     color:white;
@@ -192,11 +196,11 @@
     border-radius: 20px;
     font-family:'Source Han Sans JP'
     `;
-    const tyu = css`background-color: #EA616F;`;
-    const kou = css`background-color: #008CCF;`;
-    const nisi = css`background-color: #F5A21B;`;
-    const ken = css`background-color: #C2D95C;`;
-    const kousya = css`
+  const tyu = css`background-color: #EA616F;`;
+  const kou = css`background-color: #008CCF;`;
+  const nisi = css`background-color: #F5A21B;`;
+  const ken = css`background-color: #C2D95C;`;
+  const kousya = css`
     margin-top:4px;
     margin-bottom:4px;
     `;
