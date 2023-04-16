40dc87cfbe9d46bc9d8d672502e1e1c191a8005d
watcol <potfman@gmail.com>
2023-04-16 18:30:56 +0900

    ダウンロード実装

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -1,7 +1,62 @@
 <MakeHead name="ダウンロード" description="第77回灘校文化祭「Splash!」のパンフレットや校内マップなどのダウンロードページです。" />
-<ComingSoon/>
+<div class={containerStyle}>
+  <Title size={['4rem', '3rem']}>DOWNLOAD</Title>
+  <div class={listStyle}>
+    {#each items as item}
+      <Item {item}></Item>
+    {/each}
+  </div>
+</div>
 
 <script lang="ts">
-  import ComingSoon from '$lib/utils/ComingSoon.svelte';
+  import { css } from '@emotion/css';
   import MakeHead from '$lib/utils/MakeHead.svelte';
+  import Title from '$lib/utils/Title.svelte';
+  import Item, { type DownloadItem } from '$lib/download/Item.svelte';
+  import { responsive } from '$lib/styles/utils';
+
+  const items: DownloadItem[] = [
+    {
+      name: 'パンフレット',
+      file: 'pamphlet.pdf',
+      size: '?.?MB',
+    },
+    {
+      name: '校内マップ',
+      file: 'map.pdf',
+      size: '?.?MB',
+    },
+  ];
+
+  const containerStyle = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    ${responsive(`
+      width: 740px;
+      margin-left: calc(50% - 370px);
+      margin-top: 100px;
+      margin-bottom: 100px;
+    `, `
+      width: calc(100% - 40px);
+      margin-left: 20px;
+      margin-top: 35px;
+      margin-bottom: 35px;
+    `)}
+  `;
+
+  const listStyle = css`
+    display: flex;
+    width: 100%;
+
+    ${responsive(`
+      margin-top: 90px;
+      justify-content: space-between;
+    `, `
+      margin-top: 50px;
+      flex-direction: column;
+      gap: 10px;
+    `)}
+  `;
 </script>
